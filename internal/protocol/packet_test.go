a84e070ccb80e85e4839bf0686cf60c8b1796336
dplcz <1061281835@qq.com>
2026-02-10 00:32:11 +0800

    feat: base connect

--- internal/protocol/packet_test.go
+++ internal/protocol/packet_test.go
@@ -8,12 +8,10 @@ import (
 func TestGamePacket_Encode_Decode(t *testing.T) {
 
 	gp := NewGamePacket(3, 2, 1, []byte("abcdefg"))
-	encodedData, err := gp.Encode()
-	if err != nil {
-		t.Error(err.Error())
-	}
+	encodedData := gp.Encode()
+
 	fmt.Println(encodedData)
-	err = gp.Decode(encodedData)
+	err := gp.Decode(encodedData)
 	if err != nil {
 		t.Error(err.Error())
 	}
