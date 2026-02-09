f0a9d3200788db50ced698ada9675885b6a2f621
dengpan <dengpan@acme.red>
2026-02-09 19:00:48 +0800

    feat: add packet and errorcode

---
+++ go.mod
@@ -0,0 +1,8 @@
+module game_tun
+
+go 1.25.6
+
+require (
+	golang.org/x/sys v0.0.0-20211103235746-7861aae1554b // indirect
+	golang.zx2c4.com/wintun v0.0.0-20230126152724-0fa3db229ce2 // indirect
+)
---
+++ internal/errorcode/errorcode.go
@@ -0,0 +1,12 @@
+package errorcode
+
+import "errors"
+
+var (
+	// InvalidMagic 非法魔数
+	InvalidMagic = errors.New("invalid magic")
+	// PayloadMismatch 数据长度不匹配
+	PayloadMismatch = errors.New("payload length mismatch")
+	// PacketTooShort 包长度过短
+	PacketTooShort = errors.New("packet too short")
+)
