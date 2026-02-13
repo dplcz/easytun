cccd7d241f04e98ed7e15d45f8b5a20831a93279
dplcz <1061281835@qq.com>
2026-02-14 02:06:54 +0800

    fix:fix client bug and support client multi-worker

--- internal/config/config.go
+++ internal/config/config.go
@@ -13,6 +13,8 @@ var ServerPort int
 var DeviceName string
 var ReadTimeout time.Duration
 var PingTime time.Duration
+var SendWorkers int
+var RecvWorkers int
 
 type Configuration struct {
 	ServerIp    string        `json:"server_ip"`
@@ -20,6 +22,8 @@ type Configuration struct {
 	DeviceName  string        `json:"device_name"`
 	ReadTimeout time.Duration `json:"read_timeout"`
 	PingTime    time.Duration `json:"ping_time"`
+	SendWorkers int           `json:"send_workers"`
+	RecvWorkers int           `json:"recv_workers"`
 }
 
 func InitConfig() {
@@ -34,5 +38,14 @@ func InitConfig() {
 	DeviceName = conf.DeviceName
 	ReadTimeout = conf.ReadTimeout
 	PingTime = conf.PingTime
+	SendWorkers = conf.SendWorkers
+	if conf.SendWorkers < 1 {
+		conf.SendWorkers = 1
+	}
+	RecvWorkers = conf.RecvWorkers
+	if conf.RecvWorkers < 1 {
+		conf.RecvWorkers = 1
+	}
+
 	log.Println("初始化配置成功!")
 }
